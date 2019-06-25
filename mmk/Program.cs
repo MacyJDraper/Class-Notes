0179e474233d8d3538d6aa5f44d5c9a733937ad4
Macy Draper <Macy>
2019-06-25 09:31:33 -0500

    06-25-19

---
+++ mmk/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace mmk
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] values = { 4, 7, 12, 5, 8 };
+
+            int thirNumber = values[2];
+            Console.WriteLine(thirNumber);
+            
+        }
+    }
+}
